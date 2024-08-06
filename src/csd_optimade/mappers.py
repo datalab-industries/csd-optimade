3de6ffd50aaa7e38a0bc28a4349e55c21b0758ba
Matthew Evans <git@ml-evs.science>
2024-08-06 10:31:36 +0100

    Sequential db read parallel processing using CIF/ASE

--- src/csd_optimade/mappers.py
+++ src/csd_optimade/mappers.py
@@ -1,7 +1,11 @@
+from __future__ import annotations
+from collections.abc import Generator
 from optimade.models import StructureResource
+import os
+import time
+import itertools
 from optimade.adapters.structures.ase import from_ase_atoms
 import ase.io
-import time
 import warnings
 import json
 import tqdm
@@ -24,39 +28,63 @@ def from_csd_entry(entry: ccdc.entry.Entry) -> StructureResource:
     )
 
 
-def from_csd_database(reader):
-    for entry in reader:
+def from_csd_database(
+    reader, range_=itertools.count()
+) -> Generator[tuple[float, str | Exception]]:
+    chunked_structures = [entry for entry in [reader[r] for r in range_]]
+    for entry in chunked_structures:
+        # elapsed = -1
+        # start = time.monotonic_ns()
         try:
-            start = time.monotonic_ns()
             optimade = from_csd_entry(entry)
-            elapsed = time.monotonic_ns() - start
+            # elapsed = time.monotonic_ns() - start
         except Exception:
-            yield elapsed / 1e9, RuntimeError(f"Bad entry: {entry.identifier!r}")
+            # elapsed = time.monotonic_ns() - start
+            yield RuntimeError(f"Bad entry: {entry.identifier!r}")
 
-        dct = optimade.model_dump()
+        dct = optimade.model_dump()  # type: ignore
         dct["attributes"].pop("_ase_spacegroup", None)
-        yield elapsed / 1e9, json.dumps(dct)
+        yield json.dumps(dct)
+
+
+# def handle_chunk(chunk_id: int, range_: Generator = None):
+def handle_chunk(args):
+    run_name = "test"
+    chunk_id, range_ = args
+    with open(f"{run_name}-optimade-{chunk_id}.jsonl", "w") as f:
+        # with open(f"{run_name}-timings-{chunk_id}.csv", "w") as timings:
+        desc_string = f"CSD -> OPTIMADE ({chunk_id:7d} (PID: {os.getpid()})"
+        with tqdm.tqdm(
+            iterable=None,
+            total=100,
+            delay=chunk_id,
+            position=chunk_id,
+            maxinterval=0.5,
+            leave=False,
+            desc=desc_string,
+        ) as pbar:
+            for entry in from_csd_database(ccdc.io.EntryReader("CSD"), range_):
+                if isinstance(entry, Exception):
+                    # pbar.set_description(
+                    #     desc_string
+                    #     + f"({bad_count} bad entries so far: {bad_count/total_count:.2%})"
+                    # )
+                    continue
+                # timings.write(f"{json.loads(entry)['id']}, {timer:.2f}\n")
+                else:
+                    f.write(entry + "\n")
+                pbar.update(1)
 
 
 if __name__ == "__main__":
-    bad_count = 0
-    total_count = 0
-    with open("optimade.jsonl", "w") as f:
-        with open("timings.csv", "w") as timings:
-            for timer, entry in (
-                pbar := tqdm.tqdm(
-                    from_csd_database(ccdc.io.EntryReader("CSD")),
-                    ncols=120,
-                    total=int(1.29e7),
-                    desc="CSD -> OPTIMADE",
-                )
-            ):
-                total_count += 1
-                if isinstance(entry, Exception):
-                    bad_count += 1
-                    pbar.set_description(
-                        f"CSD -> OPTIMADE ({bad_count} bad entries so far: {bad_count/total_count:.2%})"
-                    )
-                    continue
-                timings.write(f"{json.loads(entry)['id']}, {timer:.2f}\n")
-                f.write(entry + "\n")
+    from multiprocessing import Pool
+    import sys
+
+    run_name = sys.argv[1]
+    pool_size = 8
+    chunk_size = 100
+    num_chunks = int(1.29e7) // chunk_size
+    ranges = (range(i * chunk_size, (i + 1) * chunk_size) for i in range(num_chunks))
+
+    with Pool(pool_size) as pool:
+        results = pool.map(handle_chunk, enumerate(ranges), chunksize=1)
