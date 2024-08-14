9951ff7dab5283438e1311d4657601817fddeb05
Matthew Evans <git@ml-evs.science>
2024-08-14 19:23:53 +0100

    Tidy up parallel pool

--- src/csd_optimade/mappers.py
+++ src/csd_optimade/mappers.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 from collections.abc import Generator
 from optimade.models import StructureResource
 import os
-import time
 import itertools
 from optimade.adapters.structures.ase import from_ase_atoms
 import ase.io
@@ -33,13 +32,9 @@ def from_csd_database(
 ) -> Generator[tuple[float, str | Exception]]:
     chunked_structures = [entry for entry in [reader[r] for r in range_]]
     for entry in chunked_structures:
-        # elapsed = -1
-        # start = time.monotonic_ns()
         try:
             optimade = from_csd_entry(entry)
-            # elapsed = time.monotonic_ns() - start
         except Exception:
-            # elapsed = time.monotonic_ns() - start
             yield RuntimeError(f"Bad entry: {entry.identifier!r}")
 
         dct = optimade.model_dump()  # type: ignore
@@ -52,7 +47,6 @@ def handle_chunk(args):
     run_name = "test"
     chunk_id, range_ = args
     with open(f"{run_name}-optimade-{chunk_id}.jsonl", "w") as f:
-        # with open(f"{run_name}-timings-{chunk_id}.csv", "w") as timings:
         desc_string = f"CSD -> OPTIMADE ({chunk_id:7d} (PID: {os.getpid()})"
         with tqdm.tqdm(
             iterable=None,
@@ -60,28 +54,27 @@ def handle_chunk(args):
             delay=chunk_id,
             position=chunk_id,
             maxinterval=0.5,
-            leave=False,
+            leave=True,
             desc=desc_string,
         ) as pbar:
             for entry in from_csd_database(ccdc.io.EntryReader("CSD"), range_):
                 if isinstance(entry, Exception):
-                    # pbar.set_description(
-                    #     desc_string
-                    #     + f"({bad_count} bad entries so far: {bad_count/total_count:.2%})"
-                    # )
+                    pbar.set_description(
+                        desc_string
+                        + f"({bad_count} bad entries so far: {bad_count/total_count:.2%})"
+                    )
                     continue
-                # timings.write(f"{json.loads(entry)['id']}, {timer:.2f}\n")
                 else:
                     f.write(entry + "\n")
                 pbar.update(1)
 
 
-if __name__ == "__main__":
+def main():
     from multiprocessing import Pool
     import sys
 
     run_name = sys.argv[1]
-    pool_size = 8
+    pool_size = 4
     chunk_size = 100
     num_chunks = int(1.29e7) // chunk_size
     ranges = (range(i * chunk_size, (i + 1) * chunk_size) for i in range(num_chunks))
