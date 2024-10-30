510ed6ac6b6e2709518e1cacc5e43991e66bf515
Matthew Evans <git@ml-evs.science>
2024-10-30 14:00:06 +0000

    Add ability to use a production MongoDB

--- src/csd_optimade/serve.py
+++ src/csd_optimade/serve.py
@@ -9,7 +9,14 @@ def cli():
 
     parser = argparse.ArgumentParser()
     parser.add_argument("jsonl_path", type=str, default="optimade.jsonl")
-    parser.add_argument("--port", type=int, default=5000)
+    parser.add_argument(
+        "--port", type=int, default=5000, help="Port to run the OPTIMADE API on."
+    )
+    parser.add_argument(
+        "--mongo-uri",
+        type=str,
+        help="An optional MongoDB URI to use, instead of the in-memory database.",
+    )
     args = parser.parse_args()
 
     jsonl_path = Path(args.jsonl_path)
@@ -21,5 +28,28 @@ def cli():
     else:
         jsonl_path = jsonl_path.parent
 
-    optimake_server = OptimakeServer(jsonl_path, args.port)
+    # Allow user to specify a real MongoDB
+    mongo_uri = args.mongo_uri
+    if mongo_uri:
+        import logging
+
+        import pymongo
+
+        logging.getLogger("pymongo").setLevel(logging.WARNING)
+        test_client = None
+        try:
+            test_client = pymongo.MongoClient(mongo_uri, serverSelectionTimeoutMS=1000)
+        except pymongo.ServerSelectionTimeoutError:
+            pass
+        if test_client and not test_client.server_info():
+            raise ValueError(
+                f"Could not connect to MongoDB using the provided URI: {mongo_uri}"
+            )
+
+    optimake_server = OptimakeServer(
+        jsonl_path,
+        args.port,
+        mongo_uri=mongo_uri,
+        database_backend="mongodb" if mongo_uri else "mongomock",
+    )
     optimake_server.start_api()
