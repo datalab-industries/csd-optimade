ed9c61576e857f53897e9286fc2da75b18544f44
Matthew Evans <git@ml-evs.science>
2025-01-30 21:37:45 +0000

    Treat most warnings as errors, and tweak tests to avoid false positives

--- tests/test_mappers.py
+++ tests/test_mappers.py
@@ -1,4 +1,3 @@
-import re
 import warnings
 from typing import TYPE_CHECKING
 
@@ -26,10 +25,10 @@ def check_entry(
         entry.crystal.asymmetric_unit_molecule.atoms
     )
 
-    a, b, c = entry.crystal.cell_lengths
-    alpha, beta, gamma = entry.crystal.cell_angles
-    cell = cellpar_to_cell([a, b, c, alpha, beta, gamma])
     if resource.attributes.lattice_vectors:
+        a, b, c = entry.crystal.cell_lengths
+        alpha, beta, gamma = entry.crystal.cell_angles
+        cell = cellpar_to_cell([a, b, c, alpha, beta, gamma])
         np.testing.assert_array_almost_equal(
             cell, resource.attributes.lattice_vectors, decimal=5
         )
@@ -44,29 +43,6 @@ def check_entry(
                 f"{exc} for {entry.identifier}",
                 RuntimeWarning,
             )
-
-    formula_dct = {}
-    for e in (
-        entry.crystal.asymmetric_unit_molecule.formula.strip("(").strip(")n").split(" ")
-    ):
-        matches = re.match(r"([a-zA-Z]+)([0-9]*)", e)
-        if matches:
-            species, count = matches.groups()
-            formula_dct[species] = int(count) if count else 1
-
-    formula_str: str = ""
-    for e in sorted(formula_dct):
-        formula_str += f"{e}{formula_dct[e] if formula_dct[e] > 1 else ''}"
-
-    try:
-        assert formula_str == resource.attributes.chemical_formula_reduced
-    except AssertionError as exc:
-        if warn_only:
-            warnings.warn(
-                f"{exc} for {entry.identifier}",
-                RuntimeWarning,
-            )
-
     try:
         if entry.publications:
             assert resource.relationships.references is not None
